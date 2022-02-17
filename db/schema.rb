eb3fd701415d07ed86ae344548553f7425697991
ericaohashi <m.0084erica.m.july@gmail.com>
2022-02-17 16:45:07 +0900

    issue3~6 の修正

--- db/schema.rb
+++ db/schema.rb
@@ -19,10 +19,4 @@ ActiveRecord::Schema.define(version: 2022_02_16_080939) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "plan"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
